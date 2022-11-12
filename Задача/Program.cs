e62b0f906dcf9cd9abf938fc88ce3b469b7aefea
Alex <bazz3k@ya.ru>
2022-11-12 14:06:49 +0300

    добавил функцию вывода

--- Задача/Program.cs
+++ Задача/Program.cs
@@ -51,3 +51,19 @@ void FillArray(string[] resultArray, string[] array)
     }
 }
 
+void PrintArray(string[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1)
+            Console.Write($"{array[i]}, ");
+        else
+            Console.Write($"{array[i]}]");
+    }
+}
+
+PrintArray(array);
+FillArray(resultArray,array);
+Console.Write(" => ");
+PrintArray(resultArray);
